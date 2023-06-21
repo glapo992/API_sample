ff9ba86a32f5ee663d7024d003128797249c1b83
glapo992 <giuliolapovich@yahoo.it>
2023-06-21 11:17:32 +0200

    add stuff

---
+++ client/req_to_restapi.py
@@ -0,0 +1,20 @@
+from requests import put, get
+
+def add_todo():
+    try:
+        todo_id=input('id ->')
+        todo_data=input('note ->')
+        put('http://127.0.0.1:5800/{}'.format(todo_id), data={"note":todo_data}).json()
+        return 'item inserted'
+    except ConnectionError('connection error'):
+        return ConnectionError
+
+def read_todo():
+    todo_id=input('id of item to read->')
+    return get('http://127.0.0.1:5800/{}'.format(todo_id)).json()
+
+
+
+print(add_todo())
+
+print(read_todo())
--- server/restful_api.py
+++ server/restful_api.py
@@ -12,22 +12,34 @@ class Helloworld(Resource):
         return{'Hello':'World'}
     
 
+# correctly routing an api with restful package
+api.add_resource(Helloworld, '/hello')
 
+#-----------------------------------------------------------
+# creation of a TODO list where the key is a todo_id
+# to test from terminal run:
+# curl http://127.0.0.1:5800/todo_id_1 -d "note=Remember the milk" -X PUT -> insert item
+# or
+# curl http://127.0.0.1:5800/todo_id_1 -> display item inserted
 todos = {}
 
-class TooSimple(Resource):
+class TodoSimple(Resource):
     def get(self, todo_id):
+        """returns the key:value pair of the corresponding ID """
         return {todo_id : todos[todo_id]}
 
     def put(self, todo_id):
+        """allows to add new items to the dictionary """
         todos[todo_id] = request.form['note']
         return {todo_id : todos[todo_id]}
 
 
+class DullClass1(Resource):
+    def get(self):
+        # return the dict and a respons code (if not specified = 200) and a custom header 
+        return {'key' : 'value'}, 201, {'Etag' : 'custom string'}
 
-# correctly routing an api with restful package
-api.add_resource(Helloworld, '/hello')
-api.add_resource(TooSimple, '/<string:todo_id>')
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 
 if __name__ == '__main__':

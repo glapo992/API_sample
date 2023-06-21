4a2f5c0e9e7f4b5120d23f1145486bd163e7ccc3
glapo992 <giuliolapovich@yahoo.it>
2023-06-21 09:19:54 +0200

    fixed __str__

--- client/request_sender.py
+++ client/request_sender.py
@@ -4,7 +4,7 @@ import json
 rand_str = "text container 2"
 
 url = "http://localhost:5500/process_text"  #ip address and the open port of the server target
-
+# send a hard coded string and have it back modified
 response = requests.post(url=url, data=rand_str)
 print(response.text)
 
@@ -18,15 +18,16 @@ class Person:
 
 
 def send_request():
-    name = input('daghe un nome-> ')
-    surname = input('daghe un cognome-> ')
-    given_surname = surname
+    name = input('give him a name-> ')
+    surname = input('and a surname-> ')
+
     person = Person(name = name, surname=surname) #istance of the object 
+    given_surname = person.surname
     json_p = json.dumps(person.__dict__) #automatically creates a dict from person obj with all the attr
     url_p = "http://localhost:5500/person"  #ip address of the server target
     headers = {"Content-Type":"application/json"}
     response = requests.post(url=url_p, data=json_p, headers=headers)
-    print('{}, ma prima mi conoscevi come {} {}'.format(response.text,person.name, given_surname))
+    print('{}, but before I was known as {} {}'.format(response.text,person.name, given_surname))
 
 send_request()
 
